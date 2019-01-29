bb8d057935bdcd7eb6824f45c6de57a149e27fcb
Anna Smolentzov <annasmolentzov@gmail.com>
2019-01-28 19:55:40 -0700

    adds test for github service

---
+++ spec/services/github_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe GithubService do
+  it 'exists' do
+    user = create(:user)
+    github_service = GithubService.new(user)
+    
+    expect(github_service).to be_a(GithubService)
+  end
+  
+  it 'can return repositories' do
+    user = create(:user)
+    github_service = GithubService.new(user)
+    
+    repos = github_service.repositories_by_user
+    
+    expect(repos.count).to eq(5)
+    expect(repos.first).to have_key(:name)
+    expect(repos.first).to have_key(:url)
+  end
+end
