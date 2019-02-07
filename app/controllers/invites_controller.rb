12e7011b3d6d5f7aa1f95c5ac8283be76161c4ac
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-07 16:06:56 -0700

    Refactors invites controller create method
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -4,14 +4,22 @@ class InvitesController < ApplicationController
   end
   
   def create
-    presenter = InvitePresenter.new(current_user, params[:github_handle])
-    
+    send_invite
+    redirect_to dashboard_path
+  end
+  
+  private
+  
+  def presenter
+    @_presenter ||= InvitePresenter.new(current_user, params[:github_handle])
+  end
+  
+  def send_invite
     if presenter.invitee_info[:email]
       InviterMailer.invite(presenter.invitee_info, presenter.inviter_info).deliver_now
       flash[:notice] = 'Successfully sent invite!'
     else
       flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
     end
-    redirect_to dashboard_path
   end
 end
