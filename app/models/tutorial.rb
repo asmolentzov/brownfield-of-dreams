e2697f93dd4ebb7271441785ff31a28d828f65ba
Maddie Jones <maddyg91@gmail.com>
2019-02-07 14:30:48 -0700

    Add validations to Tutorial model
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,7 +2,10 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
-  
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :thumbnail
+
   def self.bookmarked(user)
     Tutorial.includes(videos: :user_videos)
             .where(user_videos: {user_id: user})
