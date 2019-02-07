42411a66d2792596c6dd3ea5fd4ffa83d74939c9
Maddie Jones <maddyg91@gmail.com>
2019-02-06 19:45:20 -0700

    Tutorial creation working. Sad path not tested
    Co-authored-by: Anna Smolentzov <6686861+asmolentzov@users.noreply.github.com>

--- app/controllers/admin/tutorials_controller.rb
+++ app/controllers/admin/tutorials_controller.rb
@@ -4,9 +4,13 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   def create
+    @tutorial = Tutorial.create(create_tutorial_params)
+    flash[:success]= "Successfully created tutorial."
+    redirect_to tutorial_path(@tutorial.id)
   end
 
   def new
+    @admin = User.find(current_user.id)
     @tutorial = Tutorial.new
   end
 
@@ -19,7 +23,12 @@ class Admin::TutorialsController < Admin::BaseController
   end
 
   private
+
   def tutorial_params
     params.require(:tutorial).permit(:tag_list)
+  end
+
+  def create_tutorial_params
+    params.require(:tutorial).permit(:title, :description, :thumbnail)
   end
 end
