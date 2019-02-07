922a05304bb3f165781a0a4c4d264a8ce3bb1d16
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-07 16:37:07 -0700

    Refactors admin videos controller
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -11,26 +11,31 @@ class Admin::VideosController < Admin::BaseController
 
   def create
     begin
-      tutorial  = Tutorial.find(params[:tutorial_id])
-      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
-      video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
-
-      video.save
-
-      flash[:success] = "Successfully created video."
+      create_video
     rescue # Sorry about this. We should get more specific instead of swallowing all errors.
       flash[:error] = "Unable to create video."
     end
-
     redirect_to edit_admin_tutorial_path(id: tutorial.id)
   end
 
   private
-    # def video_params
-    #   params.permit(:position)
-    # end
-
+  
+    def tutorial
+      @_tutorial ||= Tutorial.find(params[:tutorial_id])
+    end
+    
+    def create_video
+      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
+      video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+      video.save
+      flash[:success] = "Successfully created video."
+    end
+    
     def new_video_params
       params.require(:video).permit(:title, :description, :video_id, :thumbnail)
     end
+    
+    # def video_params
+    #   params.permit(:position)
+    # end
 end
