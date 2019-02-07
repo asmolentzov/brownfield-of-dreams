d05678f683a8e979648f85897b58602eb2c0a6c6
Anna Smolentzov <annasmolentzov@gmail.com>
2019-02-07 16:24:05 -0700

    Refactors sessions controller create method
    Co-authored-by: Maddie Jones <22802346+maddyg91@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to dashboard_path
+      create_session
     else
-      flash[:error] = "Looks like your email or password is invalid"
-      render :new
+      login_error
     end
   end
 
@@ -19,4 +16,19 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+  
+  def user
+    @_user = User.find_by(email: params[:session][:email])
+  end
+  
+  def create_session
+    session[:user_id] = user.id
+    redirect_to dashboard_path
+  end
+  
+  def login_error
+    flash[:error] = "Looks like your email or password is invalid"
+    render :new
+  end
 end
